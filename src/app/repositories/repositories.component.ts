0aadafafb55392d357299937021c916ed563d3c2
emmanuelsw <edhgt89@hotmail.com>
2017-01-06 19:05:01 -0500

    initial app

---
+++ src/app/repositories/repositories.component.ts
@@ -0,0 +1,44 @@
+import { Component, OnInit } from '@angular/core';
+
+import { RepositoriesService } from './repositories.service';
+
+@Component({
+	selector: 'app-repositories',
+	templateUrl: './repositories.component.html',
+	styleUrls: ['./repositories.component.css']
+})
+
+
+export class RepositoriesComponent implements OnInit {
+
+	/* Propiedades */
+	repositories : any = [];
+	repository : IRepository = {name: "", description: ""};
+	newRepository : IRepository = {name:"", description:""};
+
+	constructor(private repoService : RepositoriesService) { }
+
+	ngOnInit() {
+
+		this.repoService.getRepos().subscribe((data) => {
+			this.repositories = data.json();
+			this.repository = this.repositories[0];
+		});
+
+	}
+
+	setMainRepository(repository) {
+		this.repository = repository;
+	}
+
+	addNewRepo() {
+		this.repositories.unshift(this.newRepository)
+		this.newRepository = {name:"", description:""}
+	}
+
+}
+
+interface IRepository{
+	name : string,
+	description : string
+}
